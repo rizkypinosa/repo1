049c493dbe7442a2b71143275359cdc60706d675
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-13 02:25:57 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,16 +1,10 @@
 <!DOCTYPE html>
-<html lang="en-US">
-<link rel="stylesheet" type="text/css" href="file.css">
-<p><b>Update data dalam: <span id="timer">0</span> Detik</b></p>
-<table id="coins">
-  <tr>
-    <th>Pairs</th>
-    <th>Harga</th>
-    <th>Beli</th>
-    <th>jual</th>
-    <th>Tertinggi 24h</th>
-    <th>Terendah 24h</th>
-  </tr>
-</table>
-<script web="file.js"></script>
+<html>
+<head>
+        <title>B.S.M Dockerio</title>
+</head>
+<body>
+        <h1>Update Harga Coin</h1>
+        <center><iframe src="https://indodax.com/chart/TRXIDR/" height="1000px" width="1000px"></>
+</body>
 </html>
