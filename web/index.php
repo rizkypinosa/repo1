c7cd9c29b20ae9ac69bbd6438f1a0011d367e0c1
rizkypinosa <95034884+rizkypinosa@users.noreply.github.com>
2021-11-25 21:02:49 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <title>B.S.M Dockerio</title>
-</head>
-<body>
-        <h1>Update Harga Coin</h1>
-        <center><iframe src="https://indodax.com/chart/TRXIDR/" height="1000px" width="1000px"></>
-</body>
-</html>
+<?php
+echo "sangerdingin";
+?>
